f853854882143935b77455aeddd1f8f52833594b
Sebastian M <ra3gan1991@gmail.com>
2022-05-01 11:30:46 +0200

    Firebase connection configuration and basic context generation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Landpage from "./components/Landpage";
+import {AuthProvider} from "./context/authContext";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landpage />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/context/authContext.js
@@ -0,0 +1,19 @@
+import { createContext , useContext } from "react";
+
+const authContext = createContext();
+
+export const useAuth = ()=>{
+    const context = useContext(authContext);
+    return context;
+}
+
+export const AuthProvider = ({children})=>{
+    const user = {
+        login:true
+    }
+    return(
+        <authContext.Provider value={{user}}>
+            {children}
+        </authContext.Provider>
+    ) 
+}
